d6d0ae784e50949aee8154d4678cea6788384c21
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2019-05-22 23:31:12 -0300

    Criando a barra superior
    Criando a barra superior.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
+import React, { Component } from 'react';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {
+    AppRegistry
+} from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+//importar o componente barra de navegação
+import BarraNavegacao from './src/components/BarraNavegacao';
+
+export default class app5 extends Component {
+    render() {
+        return (
+            <BarraNavegacao />
+        );
+    }
+}
+
+AppRegistry.registerComponent('app5', () => app5);
