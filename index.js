ef704ea572b481180ac80e8e32ddfcc2d8a27dc6
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2019-05-28 23:21:38 -0300

    Navigator
    Iniciando com o Navigator.

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ import {
     AppRegistry
 } from 'react-native';
 
+import { Navigator } from 'react-native-deprecated-custom-components';
+
 //importar o componente 
-//import CenaPrincipal from './src/components/CenaPrincipal';
+import CenaPrincipal from './src/components/CenaPrincipal';
 import CenaClientes from './src/components/CenaClientes';
 
 export default class app5 extends Component {
     render() {
         return (
-            <CenaClientes />
+            <Navigator 
+                initialRoute={{ id: 'a'}}
+                renderScene={(route, navigator) => {
+                    /*Definir a cena com base na rota */
+                    if (route.id === 'a'){
+                        //Exibir a cenaPrincipal
+                        return(<CenaPrincipal />);
+                    }
+                    if (route.id === 'b'){
+                        //Exibir a cenaClientes
+                        return(<CenaClientes />);
+                    }
+                }}
+            />
         );
     }
 }
